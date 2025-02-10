089e00983f81819905a852cabbb057fd90807130
mdolataGIT <maciejdolata69@gmail.com>
2025-02-10 13:32:49 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         TEST23
+         TEST234
         </p>
         <a
           className="App-link"
