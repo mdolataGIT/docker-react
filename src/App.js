b8a84262f093aab24d6a09f1ca38c97a9f929ff8
mdolataGIT <maciejdolata69@gmail.com>
2025-02-12 16:25:11 +0100

    Text change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         TEST234
+         Change
         </p>
         <a
           className="App-link"
